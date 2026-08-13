a80c52762d7558c2412ab9f9c5d24d711afdbebd
zydwz2001 <118287658+zydwz2001@users.noreply.github.com>
2026-08-13 21:32:08 +0800

    Arrange mobile setup buttons by step

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.zydwz2001.wifiotprelay"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "2.1.6"
+        versionCode = 28
+        versionName = "2.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
